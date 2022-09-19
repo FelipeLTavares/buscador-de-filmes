82fa031815f64855d42dc0f73ef6dcf443db60f0
Felipe Tavares <felipelimatavares96@gmail.com>
2022-09-19 19:48:18 -0300

    Layout geral e dos cards refeitos + adição de media queries para dar reponsividade ao design

--- src/components/cartao-filme.vue
+++ src/components/cartao-filme.vue
@@ -1,62 +1,72 @@
 <template>
   <div class="container-filme">
-    <img :src="filmeUrl" :alt="filmeNome">
+    <a :href="filmeUrl"><img :src="filmeImagem" :alt="filmeNome"/></a>
+    
     <div class="containerInfo">
       <h3>{{filmeNome}}</h3>
-      <p>{{filmeSinopse}}</p>
+      <p> Gênero(s): {{filmeStringGenero}}</p>
+      <p>Linguagem: {{filmeLingua}}</p>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'cartao-filme',
-  props: {
-    filmeUrl: String,
-    filmeNome: String,
-    filmeDescricao: String
-  },
-  data() {
-    return {
-      filmeSinopse: this.filmeDescricao
+    name: 'cartao-filme',
+    props: {
+      filmeImagem: String,
+      filmeNome: String,
+      filmeUrl: String,
+      filmeLingua: String,
+      filmeGeneros: Array
+    },
+    data() {
+      return {
+        filmeStringGenero: '',
+      }
+    },
+    methods: {
+        tratarGeneros(){
+          if(this.filmeGeneros.length<1){
+            this.filmeStringGenero = 'Indefinido'
+          } else {
+            this.filmeStringGenero = this.filmeGeneros.join(', ')
+          }
+        }
+      },
+    mounted(){
+      this.tratarGeneros()
     }
-  },
-  methods: {
-    verificarSinopse(){
-      if (this.filmeDescricao.length > 100){
-        this.filmeSinopse = this.filmeDescricao.slice(0,120) + '...'
-      } 
-    }
-  },
-  mounted(){
-    this.verificarSinopse()
   }
-}
 </script>
 
 <style>
 .container-filme{
   box-sizing: border-box;
-  border: 1px solid gray;
+  background-color: #282828;
   border-radius: 5px;
-  max-width: 300px;
+  box-shadow: 2px 2px 2px 2px black;
+  width: 280px;
   max-height: 150px;
-  display: grid;
-  grid-template-columns: 2fr 3fr;
-  margin: 10px;
+  display: flex;
+  justify-content: space-between;
+  margin: 8px;
 }
 img{
   max-height: 150px;
   border-radius: 5px;
+  margin-left: 0;
 }
 .containerInfo{
   text-align: left;
+  width: 150px;
+  color: white
 }
 .containerInfo>h3{
-  font-size: 14px;
+  font-size: 16px;
   font-weight: 600;
 }
 .containerInfo>p{
-  font-size: 12px;
+  font-size: 13px;
 }
 </style>
--- src/pages/Filmes.vue
+++ src/pages/Filmes.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="container-principal">
     <header>
-        <input type="text" class="buscar" v-model="busca">
-        <button class="botao-busca" @click="getFilmes()">Procurar</button>
+        <h1>Buscador TVMaze</h1>
+        <div class="buscador">
+            <input type="text" class="buscar" v-model="busca">
+            <button class="botao-busca" @click="getFilmes()">Procurar</button>
+        </div>
     </header>
-    <aside></aside>
     <main>
         <CartaoFilme v-for="filme in listaFilmes"
-        :key="filme.show.id" :filmeNome="filme.show.name" :filmeDescricao="filme.show.summary" :filmeUrl="filme.show.image.original"
+        :key="filme.show.id" :filmeNome="filme.show.name" :filmeImagem="filme.show.image.original"
+        :filmeUrl="filme.show.url" :filmeLingua="filme.show.language" :filmeGeneros="filme.show.genres"
         />
     </main>
     <footer></footer>
@@ -29,7 +32,7 @@ export default {
     methods: {
         async getFilmes() {
             await axios.get("https://api.tvmaze.com/search/shows?q=" + this.busca)
-                .then(e => this.listaFilmes = e.data);
+                .then(e => {this.listaFilmes = e.data; console.log(e.data)});
         }
     },
     components: { CartaoFilme }
@@ -40,14 +43,37 @@ export default {
 <style>
     .container-principal{
         display: grid;
-        grid-template: auto auto auto/1fr 4fr;
-        grid-template-areas: 'h h' 'a m' 'f f';
+        grid-template: 20vh auto 20vh/1fr 4fr;
+        grid-template-areas: 'h h' 'm m' 'f f';
     }
 
     header{
         background-color: black;
         grid-area: h;
-        height: 50px;
+        display: grid;
+        grid-template-columns: 1fr 2fr 1fr;
+        align-items: center;
+    }
+
+    header > h1{
+        font-size: 24px;
+        color: white;
+        margin: 10px;
+    }
+
+    .buscador{
+        margin: 10px;
+        height: 28px;
+    }
+
+    .buscador > input{
+        height: 24px;
+        min-width: 200px
+    }
+
+    .buscador > button{
+        height: 30px;
+        font-weight: 600;
     }
 
     aside{
@@ -60,17 +86,42 @@ export default {
         min-height: 550px;
         grid-template-columns: auto auto auto;
         gap: 10px;
-        justify-content: auto;
+        justify-items: center;
     }
     footer{
         grid-area: f;
         background-color: black;
-        height: 100px;
     }
-    .teste{
-        width: 300px;
-        height: 150px;
-        background-color: red;
-        margin: auto;
-    }    
+
+    @media (max-width: 1008px){
+        .container-principal{
+            grid-template: 20vh 15vh auto 20vh/1fr 4fr;
+            grid-template-areas: 'h h' 'm m' 'm m' 'f f';
+        }
+        main{
+            grid-template-columns: auto auto;
+        }
+        header{
+            grid-template-columns: auto auto;
+        }
+    }
+    
+    @media (max-width: 770px){
+        header{
+            grid-template-columns: auto;
+            gap: 0px;
+            align-items: center;
+        }
+        main{
+            min-height: 70vh;
+            grid-template-columns: auto auto;
+        }
+    }
+
+    @media(max-width: 480px){
+        main{
+            grid-template-columns: auto;
+        }
+    }
+
 </style>
