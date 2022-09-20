c6fa39894ebefead11a170a31adc39c7fac8956b
Felipe Tavares <felipelimatavares96@gmail.com>
2022-09-20 19:47:29 -0300

    Refinamento da logica de pesquisa e novas estilizações

--- src/pages/Filmes.vue
+++ src/pages/Filmes.vue
@@ -1,19 +1,20 @@
 <template>
   <div class="container-principal">
-    <header>
+    <header ref="head">
         <h1>Buscador TVMaze</h1>
         <div class="buscador">
-            <input type="text" class="buscar" v-model="busca">
+            <input type="text" class="buscar" v-model="busca" placeholder="Pesquise aqui..." @keyup.enter="getFilmes()">
             <button class="botao-busca" @click="getFilmes()">Procurar</button>
         </div>
     </header>
-    <main>
+    <main v-if="vazio">
         <CartaoFilme v-for="filme in listaFilmes"
-        :key="filme.show.id" :filmeNome="filme.show.name" :filmeImagem="filme.show.image.original"
-        :filmeUrl="filme.show.url" :filmeLingua="filme.show.language" :filmeGeneros="filme.show.genres"
+        :key="filme.id" :filmeNome="filme.name" :filmeImagem="filme.image.medium"
+        :filmeUrl="filme.url" :filmeLingua="filme.language" :filmeGeneros="filme.genres"
         />
     </main>
-    <footer></footer>
+    <main v-else class="mensagem"> {{mensagem}} </main>
+    <footer @click="voltarTopo()"><h4>Clique aqui para voltar para o topo!</h4></footer>
   </div>
 </template>
 
@@ -25,14 +26,32 @@ export default {
     name: "filmes-view",
     data() {
         return {
-            listaFilmes: "",
-            busca: ""
+            listaFilmes: [],
+            busca: "",
+            mensagem: 'Inicie uma pesquisa!',
+            vazio: false
         };
     },
     methods: {
         async getFilmes() {
             await axios.get("https://api.tvmaze.com/search/shows?q=" + this.busca)
-                .then(e => {this.listaFilmes = e.data; console.log(e.data)});
+                .then(e => {
+                        let listaProvisoria1 = e.data;
+                        let listaProvisoria2 = [];
+                        for (let filme of listaProvisoria1){
+                            if(filme.show.image == null){
+                                continue
+                            }
+                            listaProvisoria2.push(filme.show)
+                        }
+                        listaProvisoria2.length == 0? this.vazio = false: this.vazio = true;
+                        this.listaFilmes = listaProvisoria2;
+                        this.mensagem = 'Sem resultado para esta pesquisa... :(';
+                });
+        },
+
+        voltarTopo(){
+            this.$refs['head'].scrollIntoView({behavior: "smooth"})
         }
     },
     components: { CartaoFilme }
@@ -46,51 +65,55 @@ export default {
         grid-template: 20vh auto 20vh/1fr 4fr;
         grid-template-areas: 'h h' 'm m' 'f f';
     }
-
     header{
-        background-color: black;
+        background-color: #1C1C1C	;
         grid-area: h;
         display: grid;
         grid-template-columns: 1fr 2fr 1fr;
         align-items: center;
     }
-
     header > h1{
-        font-size: 24px;
+        font-size: 30px;
         color: white;
-        margin: 10px;
     }
-
     .buscador{
-        margin: 10px;
         height: 28px;
+        margin: 20px;
     }
-
     .buscador > input{
         height: 24px;
-        min-width: 200px
+        min-width: 200px;
+        border-radius: 5px 0 0 5px;
+        padding-left: 8px;
     }
-
     .buscador > button{
-        height: 30px;
+        height: 32px;
         font-weight: 600;
-    }
-
-    aside{
-        grid-area: a;
-        background-color: gray;
+        border-radius: 0 5px 5px 0;
     }
     main{
         grid-area: m;
         display: grid;
-        min-height: 550px;
+        min-height: 60vh;
         grid-template-columns: auto auto auto;
         gap: 10px;
         justify-items: center;
     }
+    .mensagem{
+        display: grid;
+        align-items: center;
+        justify-content: center;
+    }
     footer{
         grid-area: f;
-        background-color: black;
+        background-color: #1C1C1C;
+        display: grid;
+        align-items: center;
+        justify-content: center;
+        color: white;
+    }
+    footer > h4:hover{
+        cursor: pointer;
     }
 
     @media (max-width: 1008px){
@@ -103,6 +126,7 @@ export default {
         }
         header{
             grid-template-columns: auto auto;
+            gap: 100px;
         }
     }
     
@@ -113,15 +137,25 @@ export default {
             align-items: center;
         }
         main{
-            min-height: 70vh;
+            min-height: 60vh;
             grid-template-columns: auto auto;
         }
+        header > h1{
+        font-size: 24px;
+        color: white;
+    }
     }
 
-    @media(max-width: 480px){
+    @media(max-width: 610px){
         main{
             grid-template-columns: auto;
         }
+        header > h1{
+            margin: 5px;
+            margin-top: 5px;
+        }
+        .buscador{
+            margin: 5px;
+        }
     }
-
 </style>
